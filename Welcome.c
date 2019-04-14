053ba6bf1769a02c1900ff584e19359ef2afcced
Braam Beresford <braam@braam.email>
2019-04-14 12:37:58 -0700

    Working

--- Welcome.c
+++ Welcome.c
@@ -15,30 +15,38 @@ void driveToEdge(){
       print("Hey\n");
       drive_speed(0,0);
       return;
-   } 
-  }    
+   }
+  }  
 }  
 
-int main()                                    // Main function
-{
-  high(1);
-  int yMovement;
-  int xMovement;
-  
-   drive_getTicks(&distLeft[0], &distRight[0]);
+
+void findBorder(int* yMovement, int* xMovement){
+  drive_getTicks(&distLeft[0], &distRight[0]);
    
    driveToEdge();
    drive_getTicks(&distLeft[1], &distRight[1]);
-   yMovement = distLeft[1] - distLeft[0];
+   *yMovement = distLeft[1] - distLeft[0];
    
    
    drive_goto(26, -25);
    print("Rotate 90\n");
    drive_getTicks(&distLeft[2], &distRight[2]);
-    print("Got ticks\n");
    driveToEdge();
    drive_getTicks(&distLeft[3], &distRight[3]);
-   xMovement = distLeft[3] - distLeft[2];
+   *xMovement = distLeft[3] - distLeft[2];
+}
+/*
+void turnAround(int reverseDirection){
+  int dir = reverse? -1: 1;
+  drive_goto((dir)(26), 0);
+} */ 
+int main()                                    // Main function
+{
+  high(1);
+  high(26);
+  int yMovement;
+  int xMovement;
+  findBorder(yMovement, xMovement);
   
-   print("This white board is %d by %d ticks (x/y)", xMovement, yMovement);
+  print("This white board is %d by %d ticks (x/y)", xMovement, yMovement);
 }
