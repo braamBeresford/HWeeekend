bbc138d2d0f84f924b7ae3cbf92d59029068ab4e
Braam Beresford <braam@braam.email>
2019-04-14 13:58:45 -0700

    Many bugs

--- Welcome.c
+++ Welcome.c
@@ -1,24 +1,25 @@
 #include "simpletools.h"                      // Include simple tools
 #include "abdrive.h"
+#include <stdbool.h> 
 #define FULL_ROT = 64;
 
 int distLeft[4], distRight[4];
 
+
 void driveToEdge(){
   print("Drive to edge\n");
-  drive_ramp(64, 64);
-  pause(10000);
-  drive_speed(0,0);
+  print("Inputs: %d", input(12));
+  drive_ramp(100, 100);
   while(1){
-    
+    if(!input(12)){
+      return;
+    }  
     if(input(0)){
-      print("Hey\n");
       drive_speed(0,0);
       return;
    }
   }  
-}  
-
+} 
 
 void findBorder(int* yMovement, int* xMovement){
   drive_getTicks(&distLeft[0], &distRight[0]);
@@ -26,27 +27,49 @@ void findBorder(int* yMovement, int* xMovement){
    driveToEdge();
    drive_getTicks(&distLeft[1], &distRight[1]);
    *yMovement = distLeft[1] - distLeft[0];
-   
-   
    drive_goto(26, -25);
    print("Rotate 90\n");
    drive_getTicks(&distLeft[2], &distRight[2]);
+
    driveToEdge();
    drive_getTicks(&distLeft[3], &distRight[3]);
    *xMovement = distLeft[3] - distLeft[2];
 }
-/*
-void turnAround(int reverseDirection){
+
+ 
+void switchLane(bool reverseDirection){
   int dir = reverse? -1: 1;
-  drive_goto((dir)(26), 0);
-} */ 
+  drive_goto((dir*25), ((reverse? 1: -1)*25)); // Turn 90 degrees
+  drive_goto(14,14);
+  drive_goto((dir*25), ((reverse? 1: -1)*25));
+}
+
+void clean(){
+  bool dir = false;
+  driveToEdge(dir);
+  while(true){
+    if(!input(12)){
+      return;
+    }      
+    print("Edge!\n");
+    switchLane((dir=!dir));
+    driveToEdge();
+  }    
+}  
 int main()                                    // Main function
 {
   high(1);
   high(26);
   int yMovement;
   int xMovement;
-  findBorder(yMovement, xMovement);
+  while(true){
+    if(input(12)){
+        clean();
+        //findBorder(&yMovement, &xMovement);
+    }      
+  }    
+
   
-  print("This white board is %d by %d ticks (x/y)", xMovement, yMovement);
+  //clean();
+  return 1;
 }
